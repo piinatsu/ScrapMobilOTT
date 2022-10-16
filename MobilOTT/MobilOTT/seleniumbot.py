e83996c82ebd574e8bb5e0ab659e9bdabab54187
piinatsu <idefygravities@gmail.com>
2022-10-16 11:22:07 +0700

    Finalizing last stage of selenium first dataset

--- MobilOTT/MobilOTT/seleniumbot.py
+++ MobilOTT/MobilOTT/seleniumbot.py
@@ -10,37 +10,31 @@ options = ChromeOptions()
 options.headless = True
 driver = Chrome(options=options)
 driver.get(ott_url)
+try:
+    el = driver.find_elements(By.CLASS_NAME, 'listing')
+    for elem in el:
+        listing_id = elem.get_attribute('data-listing-id')
+        title = elem.get_attribute('data-title')
+        url = elem.get_attribute('data-url')
 
-el = driver.find_elements(By.CLASS_NAME, 'listing')
-for l in el:
-    id = l.get_attribute('id')
-    title = l.get_attribute('data-title')
-    url = l.get_attribute('data-url')
-    price = l.get_attribute('data-price')
-    make = l.get_attribute('data-make')
-    model = l.get_attribute('data-model')
-    variant = l.get_attribute('data-variant')
-    year = l.get_attribute('data-year')
-    mileage = l.get_attribute('data-mileage')
-    transmission = l.get_attribute('data-transmission')
-    ad_type = l.get_attribute('data-ad-type')
+        price = elem.find_element(By.CLASS_NAME, 'listing__price').text
+        price = price.replace('Rp', '')
+        price = price.replace('.', '')
+        price = int(price)
 
-    print(id, make, model, variant, year, mileage, transmission, ad_type)
-    print(title, price, url)
-print(el)
-# id
-# data-title
-# data-url
-# data-price
-# data-default-line-text
-# data-make
-# data-model
-# data-year
-# data-mileage
-# data-transmission
-# data-ad-type
-# data-variant
-#
-# div.listing__price
+        make = elem.get_attribute('data-make')
+        model = elem.get_attribute('data-model')
+        variant = elem.get_attribute('data-variant')
+        year = int(elem.get_attribute('data-year'))
+        mileage = int(elem.get_attribute('data-mileage'))
+        transmission = elem.get_attribute('data-transmission')
+        ad_type = elem.get_attribute('data-ad-type')
+        location = elem.find_element(By.CSS_SELECTOR, 'div.listing__specs :nth-child(3)').text
 
-driver.quit()
+        print(listing_id, make, model, variant, year, mileage, transmission, price, ad_type, location)
+        print(title, url)
+        print('-----------------')
+    print(el)
+
+finally:
+    driver.quit()
